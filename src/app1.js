c872084a63be2a2d7dc3a3f1cb31a415a7eb550d
Zaid Mohammed <zaid.md221@gmail.com>
2024-11-29 20:07:52 +0530

    testing automation5

---
+++ src/app1.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => res.send("Hello World!"));
+app.listen(3000, () => console.log("Server running on port 3000"));
