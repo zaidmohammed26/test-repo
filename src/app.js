1b799301b61b8638440c3b86823280fc9bb436d9
Zaid Mohammed <zaid.md221@gmail.com>
2024-12-01 03:42:51 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(middleware);
 
 // Routes
-app.get("/hello", (req, res) => res.send("Hello sufiyaaaaaann!"));
+app.get("/hello", (req, res) => res.send("Hello sufiyaaaaaaann!"));
 
 // Start the server
 const PORT = 3000;
