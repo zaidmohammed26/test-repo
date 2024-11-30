599de6d68cde8ed85f72ef16a8d74f2e0f6b705d
Zaid Mohammed <zaid.md221@gmail.com>
2024-12-01 03:13:39 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
+// const express = require("express");
+// const middleware = require("./app1");
+// const app = express();
+// const cors = require("cors");
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use(middleware);
+// app.get("/hello", (req, res) => res.send("Hello World!"));
+// app.listen(3000, () => console.log("Server running on port 3000"));
 const express = require("express");
 const middleware = require("./app1");
+const cors = require("cors");
+
 const app = express();
 
+// Middleware for CORS and parsing JSON
+app.use(cors());
+app.use(express.json());
+
+// Custom middleware
 app.use(middleware);
-app.get("/hello", (req, res) => res.send("Hello World!"));
-app.listen(3000, () => console.log("Server running on port 3000"));
+
+// Routes
+app.get("/hello", (req, res) => res.send("Hello sufiya!"));
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
