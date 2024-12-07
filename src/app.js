75d0133691faa283a9abf957d4285060befd63dd
Zaid Mohammed <zaid.md221@gmail.com>
2024-12-07 19:09:28 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@
 // app.get("/hello", (req, res) => res.send("Hello World!"));
 // app.listen(3000, () => console.log("Server running on port 3000"));
 const express = require("express");
-const middleware = require("./app1");
+const middleware = require("auto-docs-cli/middleware");
 const cors = require("cors");
 
 const app = express();
